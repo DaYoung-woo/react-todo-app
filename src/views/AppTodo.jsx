d9ee54d596877864252cb7cc89aa618ad555dbd6
wooda0 <131967254+DaYoung-woo@users.noreply.github.com>
2023-04-30 01:12:49 +0900

    Todo 아이템 추가

--- src/views/AppTodo.jsx
+++ src/views/AppTodo.jsx
@@ -1,28 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TodoHeader from "../components/TodoHeader";
 import TodoList from "../components/TodoList";
 import TodoBottom from "../components/TodoBottom";
 
 export default function AppTodo() {
   const [mode, setMode] = useState("All");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(
+    localStorage.getItem("myTodoList")
+      ? JSON.parse(localStorage.getItem("myTodoList"))
+      : []
+  );
+
+  useEffect(() => {
+    console.log(list);
+    localStorage.setItem("myTodoList", JSON.stringify(list));
+  }, [list]);
 
   const changeMode = (mode) => {
     setMode(mode);
   };
 
   const addTodo = (text) => {
-    setList((prev) => {
-      prev.push({
-        text,
-        isComplete: false,
-      });
-    });
+    setList(list.concat({ text, isComplete: false }));
   };
   return (
     <div className="App justify-center items-center">
       <div className="rounded-xl shadow-lg card-size">
-        {mode}
         <TodoHeader changeMode={changeMode} mode={mode} />
         <TodoList list={list} />
         <TodoBottom addTodo={addTodo} />
