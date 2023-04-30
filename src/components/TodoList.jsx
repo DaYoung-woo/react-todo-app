fe5319dd7f712c28dd844e873adbc68d62b7f945
wooda0 <131967254+DaYoung-woo@users.noreply.github.com>
2023-04-30 16:21:34 +0900

    함수 분리

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,24 +1,25 @@
 import React, { useContext } from "react";
 import TodoItem from "./TodoItem";
 import ThemeContext from "../context/ThemeContext";
+
+function filterTodoList(list, mode) {
+  return mode === "All"
+    ? list
+    : list.filter(
+        (el) =>
+          (mode === "Completed" && el.isComplete) ||
+          (mode === "Active" && !el.isComplete)
+      );
+}
+
 export default function TodoList({ list, checkTodo, deleteTodo, mode }) {
   const { theme } = useContext(ThemeContext);
   return (
     <div className={`list-body ${theme}`}>
       <ul className="px-4 py-4">
-        {list.map((todo) =>
-          mode === "All" ||
-          (mode === "Active" && !todo.isComplete) ||
-          (mode === "Completed" && todo.isComplete) ? (
-            <TodoItem
-              todo={todo}
-              checkTodo={checkTodo}
-              deleteTodo={deleteTodo}
-            />
-          ) : (
-            <></>
-          )
-        )}
+        {filterTodoList(list, mode).map((todo) => (
+          <TodoItem todo={todo} checkTodo={checkTodo} deleteTodo={deleteTodo} />
+        ))}
       </ul>
     </div>
   );
