7c3b215bb045e26b9150a3e7d0467b26d37c8468
DaYoung-woo <iamwooda0@gmail.com>
2024-01-16 00:13:42 +0900

    md 파일 변경/prettier 삭제

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 ## REACT Todo App 프로젝트
 
-리액트를 공부하고 가볍게 만들어본 토이 프로젝트입니다.
+리액트를 공부하고 처음으로 만들어본 TO DO 토이 프로젝트입니다.
 
-![image](https://github.com/DaYoung-woo/react-todo-app/assets/131967254/1a10e01d-c72f-419c-b37c-2ddc6f5c58e8)
+context와 localstorage를 사용하여 다크모드를 지원하고 있습니다.
 
-처음 사이트에 접속하면 위 사진과 같이 제가 임의로 저장해 놓은 정보가 나타납니다. 
-이 정보들은 localStorage에 저장되어 있습니다
+[사이트 접속](https://fastidious-gumdrop-97466c.netlify.app)
 
-
-![image](https://github.com/DaYoung-woo/react-todo-app/assets/131967254/1a4f50a7-64f3-46ce-8bc5-d4deae4098a4)
+![image](https://github.com/DaYoung-woo/react-todo-app/assets/131967254/1a10e01d-c72f-419c-b37c-2ddc6f5c58e8)  
+![image](https://github.com/DaYoung-woo/react-todo-app/assets/131967254/1a4f50a7-64f3-46ce-8bc5-d4deae4098a4)  
 상단 오른쪽 아이콘을 클릭하면 다크모드로 사용할 수 있습니다.
+
+처음 사이트에 접속하면 제가 임의로 저장해 놓은 할 일 정보가 나타납니다.
+백엔드는 따로 사용하지 않았고 할 일에 대한 정보들은 localStorage에 저장하여 사용했습니다.
--- src/views/AppTodo.tsx
+++ src/views/AppTodo.tsx
@@ -1,64 +1,64 @@
-import React, { useState, useEffect } from 'react'
-import TodoHeader from '../components/TodoHeader'
-import TodoList from '../components/TodoList'
-import TodoBottom from '../components/TodoBottom'
-import ThemeProvider from '../components/ThemeProvider'
+import { useState, useEffect } from "react";
+import TodoHeader from "../components/TodoHeader";
+import TodoList from "../components/TodoList";
+import TodoBottom from "../components/TodoBottom";
+import ThemeProvider from "../components/ThemeProvider";
 
 interface todoItemType {
-  text: string
-  isComplete: boolean
+  text: string;
+  isComplete: boolean;
 }
 
-export default function AppTodo (): JSX.Element {
-  const [mode, setMode] = useState('All')
+export default function AppTodo() {
+  const [mode, setMode] = useState("All");
   const changeMode = (mode: string): void => {
-    setMode(mode)
-  }
+    setMode(mode);
+  };
 
   const [list, setList] = useState(() => {
-    const localTodoList: string | null = localStorage.getItem('myTodoList')
+    const localTodoList: string | null = localStorage.getItem("myTodoList");
     if (localTodoList !== null) {
-      const myTodoList: todoItemType[] = JSON.parse(localTodoList)
-      return myTodoList
+      const myTodoList: todoItemType[] = JSON.parse(localTodoList);
+      return myTodoList;
     } else {
       const TodoList: todoItemType[] = [
         {
-          text: '리액트 강의 듣기',
-          isComplete: true
+          text: "리액트 강의 듣기",
+          isComplete: true,
         },
         {
-          text: 'Todo App 만들기',
-          isComplete: true
+          text: "Todo App 만들기",
+          isComplete: true,
         },
         {
-          text: 'Youtube App 만들기',
-          isComplete: false
-        }
-      ]
-      localStorage.setItem('myTodoList', JSON.stringify(TodoList))
-      return TodoList
+          text: "Youtube App 만들기",
+          isComplete: false,
+        },
+      ];
+      localStorage.setItem("myTodoList", JSON.stringify(TodoList));
+      return TodoList;
     }
-  })
+  });
 
   useEffect(() => {
-    localStorage.setItem('myTodoList', JSON.stringify(list))
-  }, [list])
+    localStorage.setItem("myTodoList", JSON.stringify(list));
+  }, [list]);
 
   const addTodo = (text: string): void => {
-    setList(list.concat({ text, isComplete: false }))
-  }
+    setList(list.concat({ text, isComplete: false }));
+  };
 
   const checkTodo = (text: string): void => {
     setList(
       list.map((el: todoItemType) => {
-        return el.text === text ? { ...el, isComplete: !el.isComplete } : el
+        return el.text === text ? { ...el, isComplete: !el.isComplete } : el;
       })
-    )
-  }
+    );
+  };
 
   const deleteTodo = (text: string): void => {
-    setList(list.filter((el) => el.text !== text))
-  }
+    setList(list.filter((el) => el.text !== text));
+  };
 
   return (
     <ThemeProvider>
@@ -71,5 +71,5 @@ export default function AppTodo (): JSX.Element {
       />
       <TodoBottom addTodo={addTodo} />
     </ThemeProvider>
-  )
+  );
 }
